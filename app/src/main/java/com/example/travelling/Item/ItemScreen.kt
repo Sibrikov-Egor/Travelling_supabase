5d7b42a469344c6ca498a096fc6b5d5ad590b589
Egor <igor.sibrikov@mail.ru>
2025-04-02 19:13:00 +0300

    Travelling_supabase

--- app/src/main/java/com/example/travelling/Item/ItemScreen.kt
+++ app/src/main/java/com/example/travelling/Item/ItemScreen.kt
@@ -7,13 +7,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,51 +36,91 @@ fun ListItem(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center)
     {
-        Image(
-            painter = painterResource(id = R.drawable.logo), contentDescription = "",
+        Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(170.dp)
-                .clip(RoundedCornerShape(15.dp))
+                .fillMaxSize()
+                .padding(10.dp),
+            shape = RoundedCornerShape(15.dp),
 
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = "Горы",
-            color = Color.Black,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
-        Spacer(modifier = Modifier.height(5.dp))
-        Text(
-            text = "Описание",
-            color = Color.Gray,
-            fontSize = 16.sp
-        )
-        Spacer(modifier = Modifier.height(5.dp))
-        Text(
-            text = "1000 рублей",
-            color = Color.Blue,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp
-        )
+            )  {
+            LazyVerticalGrid(columns = GridCells.Fixed(2)){
+                items(10){
+                    Card(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp),
+                        shape = RoundedCornerShape(15.dp),
+
+                        )  {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo),
+                            contentDescription = "logo",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .size(64.dp)
+                                .clip(CircleShape)
+
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "Горы",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Text(
+                            text = "Описание",
+                            color = Color.Gray,
+                            fontSize = 16.sp
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Text(
+                            text = "1000 рублей",
+                            color = Color.Blue,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                    }
+                }
+//        Card(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(10.dp),
+//            shape = RoundedCornerShape(15.dp),
+//
+//            )  {
+//        Image(
+//            painter = painterResource(id = R.drawable.logo),
+//            contentDescription = "logo",
+//            contentScale = ContentScale.Crop,
+//            modifier = Modifier
+//                .padding(5.dp)
+//                .size(64.dp)
+//                .clip(CircleShape)
+//
+//        )
+//        Spacer(modifier = Modifier.height(10.dp))
+//        Text(
+//            text = "Горы",
+//            color = Color.Black,
+//            fontWeight = FontWeight.Bold,
+//            fontSize = 20.sp
+//        )
+//        Spacer(modifier = Modifier.height(5.dp))
+//        Text(
+//            text = "Описание",
+//            color = Color.Gray,
+//            fontSize = 16.sp
+//        )
+//        Spacer(modifier = Modifier.height(5.dp))
+//        Text(
+//            text = "1000 рублей",
+//            color = Color.Blue,
+//            fontWeight = FontWeight.Bold,
+//            fontSize = 18.sp
+//        )
+            }}
     }}
-//    Card(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(10.dp),
-//        shape = RoundedCornerShape(15.dp),
-//
-//    )  {
-//
-//            Image(
-//                painter = painterResource(id = R.drawable.logo),
-//                contentDescription = "logo",
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier
-//                    .padding(5.dp)
-//                    .size(64.dp)
-//                    .clip(CircleShape)
-//            )
-//        }
 
